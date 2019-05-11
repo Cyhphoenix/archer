81a793052eb66a37bb8ed82c9e63ba91a153bfea
yuanhang <pa_phoenix@staff.sina.com.cn>
2019-05-11 23:58:02 +0800

    alt deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd ~
-WORK_DIR=~/archer
 
 if [ -d archer ]; then
   rm -rf archer
@@ -8,8 +7,13 @@ fi
 
 git clone https://github.com/Cyhphoenix/archer.git
 
+WORK_DIR=~/archer
 cd $WORK_DIR
 
 mvn clean package -Dmaven.test.skip=true
+
+if [[ "`ps aux | grep 'archer-web-0.0.1-SNAPSHOT.jar' | grep -v 'grep' | wc -l`" != "0" ]]; then
+	kill -9 `ps aux |grep 'archer-web-0.0.1-SNAPSHOT.jar' | grep -v 'grep'|awk '{print $2}'`
+fi
 
 java -jar ./archer-web/target/archer-web-0.0.1-SNAPSHOT.jar > $WORK_DIR/stdout.log &
